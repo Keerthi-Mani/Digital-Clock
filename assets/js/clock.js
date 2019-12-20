6f6872466796dab6c3cfe2b96d9725edb4a67744
Keerthi Mani <carkeane.getz@gmail.com>
2019-12-20 14:07:46 -0500

    get the current time,date,day,month and year

---
+++ assets/js/clock.js
@@ -0,0 +1,35 @@
+var dateOfNow = new Date();
+// console.log(dateOfNow);
+var hours = dateOfNow.getHours();
+var minutes = dateOfNow.getMinutes();
+var seconds = dateOfNow.getSeconds();
+// console.log(hours, minutes, seconds);
+var days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+var currentDay = days[dateOfNow.getDay()];
+// console.log(currentDay);
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+var currentMonth = months[dateOfNow.getMonth()];
+// console.log(currentMonth);
+var currentYear = dateOfNow.getFullYear();
+// console.log(currentYear);
