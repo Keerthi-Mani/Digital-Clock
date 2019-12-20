68b1979245cf24d41d2f7986dfc3b92bf12b096c
Keerthi Mani <carkeane.getz@gmail.com>
2019-12-20 17:12:20 -0500

    display the day of the week in the clock

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,5 +1,6 @@
 var dateOfNow = new Date();
 // console.log(dateOfNow);
+var date = dateOfNow.getDate();
 var hours = dateOfNow.getHours();
 var minutes = dateOfNow.getMinutes();
 var seconds = dateOfNow.getSeconds();
@@ -33,7 +34,13 @@ var currentMonth = months[dateOfNow.getMonth()];
 // console.log(currentMonth);
 var currentYear = dateOfNow.getFullYear();
 // console.log(currentYear);
+
+//Displays the date, month and year in the div of the index.html
 var displayDate = document.getElementById("date-month-year");
-var displayValues = currentMonth + " " + currentYear;
-displayDate.innerText = displayValues;
+var dateValues = date + " " + currentMonth + " " + currentYear;
+displayDate.innerText = dateValues;
 // console.log(displayDate);
+
+//Displays the day in the div of the index.html
+var displayDay = document.getElementById("dayoftheweek");
+displayDay.innerText = currentDay;
